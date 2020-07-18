5bf401e65a90399daa5e2ee82c533732d78eeaab
Enbani <enbani.moore@gmail.com>
2020-07-17 21:12:20 -0400

    adding prescriptions routes

--- server/routes/rx.js
+++ server/routes/rx.js
@@ -2,6 +2,7 @@ const express = require('express');
 const _ = require('lodash');
 const moment = require('moment');
 const db = require('../db');
+const { Op } = require('sequelize');
 
 const { Rx } = db.models;
 
@@ -20,7 +21,7 @@ router.post('/', async (req, res) => {
   body.rxid = charset + numset;
 
   try {
-    const rx = await Rx.create(body)
+    let rx = await Rx.create(body)
     return res.status(200).send({payload: 'success!'})
   } catch (e) {
     return res.status(400).send({error: e || 'Error creating a prescription'});
@@ -31,21 +32,53 @@ router.get('/:phone', async (req, res) => {
   let { phone } = req.params;
 
   try {
-    const rx = await Rx.findAll({
-      where: {
-        phone
-      }
-    });
-
+    let [rx, metadata] = await db.sequelize.query(`
+      SELECT * FROM rxes
+      WHERE phone=${phone} AND (expiryDate >= NOW() OR expiryDate is NULL)
+      ORDER BY createdAt DESC LIMIT 1
+      `)
+    if (!rx) {
+      return res.status(404).send({error: 'No matching prescriptions'})
+    }
     return res.status(200).send({payload: {
-      rxid: rx[0].dataValues.rxid,
-      amount: rx[0].dataValues.amount
+      rxid: rx.rxid,
+      amount: rx.amount
     }});
   } catch (e) {
     console.log(e)
     return res.status(400).send({error: e || 'Error fetching prescription.'})
   }
+});
 
+router.patch('/:rxid', async (req, res) => {
+  let { rxid } = req.params;
+  let { market } = req.body;
+  let redeemDate = moment();
+  let expiryDate = moment().add(1, 'h');
+
+  try {
+    let [rx, metadata] = await db.sequelize.query(`
+      UPDATE rxes
+      SET redeemDate = NOW(),
+          expiryDate = NOW + INTERVAL 1 HOUR,
+          market = ${market}
+      WHERE rxid = ${rxid}
+      `)
+
+    console.log('This is rx \n\n', rx);
+
+    let [updatedRxes ,meta] = await db.sequelize.query(`
+      UPDATE rxes
+      SET expiryDate = NOW()
+      WHERE phone = ${rx.phone} AND rxid <> ${rxid} AND expiryDate is NULL
+      `)
+
+    console.log('This is updatedRxes \n\n',updatedRxes);
+
+    return res.status(200).send({payload: {rx}})
+  } catch (e) {
+    return res.status(400).send({error: e || 'Error updating prescription.'})
+  }
 })
 
 
