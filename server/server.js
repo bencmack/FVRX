06b62783c7050937d2325488bccedfa26d56c655
Benjamin Cormack <bjc9006@sis.nyp.org>
2020-07-31 19:12:15 -0400

    change path back for index.html file

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/api', routes);
 
 // Serve static files from the React app
 //app.use(express.static(path.join(__dirname, '/../client/build')));
-app.use(express.static('../build/'));
+app.use(express.static('../client/build/'));
 
 let forceVal = (process.env.ENVIRONMENT === 'development') ? true : false;
 
